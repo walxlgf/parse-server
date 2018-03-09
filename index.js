eb0f7cd1a06910dc75187f26f1935361cb115696
walxlgf <guanghui-li@163.com>
2018-03-09 21:23:07 +0800

    删除readOnlyMasterKey，clientKey，restAPIKey

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const api = new ParseServer({
   cloud: __dirname + '/cloud/main.js', // Absolute path to your Cloud Code
   appId: APP_ID,
   masterKey: MASTER_KEY,
-  readOnlyMasterKey: READ_ONLY_MASTER_KEY,
-  restAPIKey: REST_API_KEY,
-  clientKey: CLIENT_KEY,
+  // readOnlyMasterKey: READ_ONLY_MASTER_KEY,
+  // restAPIKey: REST_API_KEY,
+  // clientKey: CLIENT_KEY,
   serverURL: 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Game', 'Device', 'Pin']
