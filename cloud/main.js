5d08e75244acfc67fee79a4707795265d34f9ab1
walxlgf <guanghui-li@163.com>
2018-03-10 18:11:41 +0800

    添加了cloud code 方法wechatlogin

--- cloud/main.js
+++ cloud/main.js
@@ -2,6 +2,30 @@ Parse.Cloud.define('hello', (req, res) => {
   res.succes('hello!');
 });
 
+Parse.Cloud.define('wechatlogin', (req, res) => {
+  var code = req.params.code;
+  //获取openId 
+  Parse.Cloud.httpRequest({
+    url: 'https://api.weixin.qq.com/sns/jscode2session',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    params: {
+      appid: 'wxc14d0ff891dbbb64',
+      secret: '654f6c6559336fa79d13c85e4cb2e080',
+      js_code: code,
+      grant_type: 'authorization_code',
+    }
+  }).then(function (httpResponse) {
+    console.log(httpResponse.text);
+  }, function (httpResponse) {
+    console.error('Request failed with response code ' + httpResponse.status);
+  });
+
+  res.succes('hello!');
+});
+
+
 
 Parse.Cloud.beforeSave("Device", function (request, response) {
   const query = new Parse.Query("Device");
